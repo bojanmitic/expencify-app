6cd759f54b679dbbe654df672bffbb32a200e401
uusi <bojanmitic84@yahoo.com>
2018-04-11 11:50:54 +0300

    Setup production webpack build

--- webpack.config.js
+++ webpack.config.js
@@ -3,7 +3,7 @@ const ExtractTextPlugin = require('extract-text-webpack-plugin');
 
 module.exports = (env) => {
   const isProduction = env === 'production';
-  const CssExtract = new ExtractTextPlugin('syles.css');
+  const CSSExtract = new ExtractTextPlugin('styles.css');
 
   console.log('env', env);
   return {
@@ -19,18 +19,28 @@ module.exports = (env) => {
         exclude: /node_modules/
       }, {
         test: /\.s?css$/,
-        use: CssExtract.extract({
+        use: CSSExtract.extract({
           use: [
-            'css-loader',
-            'sass-loader'
+            {
+              loader: 'css-loader',
+              options: {
+                sourceMap: true
+              } 
+            },
+            {
+              loader: 'sass-loader',
+              options: {
+                  sourceMap: true
+              }
+            } 
           ]
         })
       }]
     },
     plugins:[
-      CssExtract
+      CSSExtract
     ],
-    devtool: isProduction ? 'source-map' : 'cheap-module-eval-source-map',
+    devtool: isProduction ? 'source-map' : 'inline-source-map',
     devServer: {
       contentBase: path.join(__dirname, 'public'),
       historyApiFallback: true
